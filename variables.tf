dfd84c8306e6d474ded5e871129fb76bc3822d55
Saikishore031 <50044342+Saikishore031@users.noreply.github.com>
2024-10-14 05:10:04 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,17 +20,14 @@ variable "aws_region" {
 variable "ami" {
    type        = string
    description = "Ubuntu AMI ID"
-   default     = "ami-0f5ee92e2d63afc18"
 }
 
 variable "instance_type" {
    type        = string
    description = "Instance type"
-   default     = "t2.micro"
 }
 
 variable "name_tag" {
    type        = string
    description = "Name of the EC2 instance"
-   default     = "My EC2 Instance"
 }
